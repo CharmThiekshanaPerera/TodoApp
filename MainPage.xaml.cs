b2d0f0feb7eb6f6d485106b9059b617f46397511
Charm Thiekshana Perera <Charmthiekshana97@outlook.com>
2025-04-21 00:31:15 +0530

    update dependncies

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,25 @@
-﻿namespace TodoApp;
+﻿using Microsoft.Maui.Controls;
+using TodoApp.Models;
+using TodoApp.ViewModels;
 
-public partial class MainPage : ContentPage
+namespace TodoApp
 {
-	int count = 0;
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (sender is CheckBox checkbox && checkbox.BindingContext is TodoItem item)
+            {
+                var viewModel = BindingContext as MainViewModel;
+                viewModel?.ToggleCompleteCommand.Execute(item);
+            }
+        }
+    }
 }
-
+// This code is part of a simple Todo application using .NET MAUI and MVVM pattern.
+// It defines the MainPage class, which is the main user interface of the application.
