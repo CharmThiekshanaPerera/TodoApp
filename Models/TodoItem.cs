296130bf9a7194d7eff5a9ac516b02cef106e573
Charm Thiekshana Perera <Charmthiekshana97@outlook.com>
2025-04-20 23:05:36 +0530

    First Commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
